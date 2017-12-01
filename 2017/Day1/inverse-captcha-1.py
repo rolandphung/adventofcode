8909eb0b9b68a548dceb8ede08c06319229c7120
Roland Phung <414053+rolandphung@users.noreply.github.com>
2017-12-01 15:41:00 -0800

    Prevent needless 0s from being populated in Day1 solutions

--- 2017/Day1/inverse-captcha-1.py
+++ 2017/Day1/inverse-captcha-1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Part 1
 digits = [int(i) for i in open("input").read().strip()]
-values = [digits[i] if digits[i] == digits[i + 1] else 0
-          for i in range(-1, len(digits) - 1)]
+values = [digits[i] 
+          for i in range(-1, len(digits) - 1)
+          if digits[i] == digits[i + 1]]
 print sum(values)
--- 2017/Day1/inverse-captcha-2.py
+++ 2017/Day1/inverse-captcha-2.py
@@ -2,6 +2,6 @@
 digits = [int(i) for i in open("input").read().strip()]
 length = len(digits)
 half = length / 2
-values = [digits[i] if digits[i] == digits[(i + half) % length] else 0
-          for i in range(len(digits))]
+values = [digits[i] for i in range(len(digits))
+          if digits[i] == digits[(i + half) % length]]
 print sum(values)
