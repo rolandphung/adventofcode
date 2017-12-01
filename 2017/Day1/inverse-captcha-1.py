bf9d87964732df5511e8b5b81f5c8eb379c41021
Roland Phung <414053+rolandphung@users.noreply.github.com>
2017-12-01 15:27:57 -0800

    Adding a comment

--- 2017/Day1/inverse-captcha-1.py
+++ 2017/Day1/inverse-captcha-1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# Part 1
 digits = [int(i) for i in open("input").read().strip()]
 values = [digits[i] if digits[i] == digits[i + 1] else 0
           for i in range(-1, len(digits) - 1)]
